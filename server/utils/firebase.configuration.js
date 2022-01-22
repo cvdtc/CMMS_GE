ad0ab0663605f98ee9949aa8e49db51c80e3405d
msaprojects <msadev96@gmail.com>
2022-01-22 13:20:29 +0700

    update sync with server

--- server/utils/firebase.configuration.js
+++
@@ -1,6 +0,0 @@
-var fcmadmin = require('firebase-admin')
-var serverKey = require('./cmmsge.firebasetoken.json')
-fcmadmin.initializeApp({
-    credential: fcmadmin.credential.cert(serverKey)
-})
-module.exports = fcmadmin
